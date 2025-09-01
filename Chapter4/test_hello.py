2dcb55bac0d475355abcd84e150b56e70bae7d1a
Raihan-chowdhury7 <raihan2cho@gmail.com>
2025-09-01 15:21:55 +0100

    combined matrix pipline error fixes

--- Chapter4/test_hello.py
+++ Chapter4/test_hello.py
@@ -3,4 +3,5 @@ import unittest
 from hello import say_hello
 
 class TestHello(unittest.TestCase):
-    self.assertEqual(say_hello(), "Hello World")
+    def test_say_hello(self):
+        self.assertEqual(say_hello(), "Hello World!")
